8aade4bfc295e023964473c080d24f708be97f38
maheshviruvuru <maheshvirkal@gmail.com>
2022-09-30 20:24:37 +0530

    new project

--- Amazon-framework/utilities/base_class.py
+++ Amazon-framework/utilities/base_class.py
@@ -1,3 +1,5 @@
+import inspect
+import logging
 import time
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path='C:\Drivers\chromedriver_win32\chromedriver.exe')
@@ -8,3 +10,12 @@ class BasePage:
     def is_element_present(self, element, timeout=3):
         time.sleep(timeout)
         return element.is_displayed()
+
+    def get_logger(self):
+        logger_name = inspect.stack()[1][3]
+        logger = logging.getLogger(logger_name)
+        fil_handler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+        fil_handler.setFormatter(formatter)
+        logger.addHandler(fil_handler)
+        logger.setLevel(logging.DEBUG)

acc52d1fdf0e5aa5e8efd2058bf5ae02e73d34b8
MaheshViruvuru <112768613+MaheshViruvuru@users.noreply.github.com>
2023-02-21 20:30:18 +0530

    updated

--- Amazon-framework/Tests/conftest.py
+++ Amazon-framework/Tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+options = webdriver.ChromeOptions()
+options.headless = True
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
 @pytest.fixture(scope='class')
